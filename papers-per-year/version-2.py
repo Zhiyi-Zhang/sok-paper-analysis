8894a1a44a39212f910f9a586ad7c74ff280bd41
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2021-04-12 10:31:50 -0700

    update

--- papers-per-year/version-2.py
+++ papers-per-year/version-2.py
@@ -3,10 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 sns.set_theme(style="darkgrid")
-fig, axes = plt.subplots(1, 3,figsize=(8,5))
+fig, axes = plt.subplots(1, 3,figsize=(8,4))
 
-# attack figure
-
+# paper figure
 df = pd.read_csv("paper-per-year.csv")
 
 sns.regplot(x="year", y="# of papers", data=df, ax=axes[0])
@@ -18,7 +17,7 @@ for line in range(0, df.shape[0]):
 
 axes[0].get_yaxis().set_visible(False)
 axes[0].set_ylim(0, 560)
-axes[0].set_xlim(1990, 2020)
+axes[0].set_xlim(1990, 2021)
 axes[0].set_xlabel('Year')
 axes[0].set_ylabel('')
 axes[0].set_title("Papers Per Year")
@@ -29,15 +28,14 @@ print(df)
 sns.lineplot(x="year", y="size", data=df, ax=axes[1], color='g', markers=True, marker="o")
 
 for line in range(0, df.shape[0]):
-     axes[1].text(df['year'][line]-3, df['size'][line]+0.1, df["tag"][line], horizontalalignment='left', size='small', color='black', weight='semibold')
+     axes[1].text(df['year'][line]-2, df['size'][line], df["tag"][line], horizontalalignment='left', size='small', color='black', weight='semibold')
 
 axes[1].get_yaxis().set_visible(False)
-axes[1].set_ylim(0, 5)
 axes[1].set_xlabel('Year')
-axes[1].set_xlim(2010, 2025)
+axes[1].set_xlim(2010, 2021)
 axes[1].set_title("Market Size (Billion$)")
 
-# paper figure
+# attack figure
 df = pd.read_csv("attacks.csv")
 sns.regplot(x="x", y="y", data=df, ax=axes[2], color='r', order=3, ci=None)
 
@@ -47,9 +45,9 @@ for line in range(0, df.shape[0]):
 axes[2].get_yaxis().set_visible(False)
 axes[2].set_ylim(0, 2600)
 axes[2].set_xlabel('Year')
-axes[2].set_xlim(2000, 2020)
+axes[2].set_xlim(2000, 2021)
 axes[2].set_title("Peak Traffic Rate (Gbps)")
 
 plt.tight_layout()
 # plt.show()
-plt.savefig('combined-2.pdf')
+plt.savefig('recent-ddos-attacks-papers.pdf')
---
+++ papers-per-year/version-3.py
@@ -0,0 +1,33 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+sns.set_theme(style="darkgrid")
+fig, ax = plt.subplots(figsize=(8,4))
+
+# attack figure
+
+df = pd.read_csv("raw-data/attack-market.csv")
+ax.set_ylim(0, 3)
+sns.regplot(x="year", y="size", data=df.query("type == 'attack'"), ax=ax, color='r', order=3, ci=None, truncate=False, label='attack traffic rate')
+sns.lineplot(x="year", y="size", data=df.query("type == 'market'"), ax=ax, color='g', markers=True, marker="o", label='market size')
+ax.legend(loc=9)
+ax.set_ylabel('Peak Traffic Rate (Tbps), Market Size ($Billion)')
+
+# paper figure
+ax2 = ax.twinx()
+df = pd.read_csv("raw-data/paper-per-year-from-2000.csv")
+ax2.grid(False)
+ax2.set_xlim(2000, 2020)
+ax2.set_ylim(100, 600)
+sns.regplot(x="year", y="# of papers", data=df, ax=ax2, truncate=False, label='papers per year')
+ax2.legend(loc=2)
+ax2.set_ylabel('Number of Papers per Year')
+ax2.set_xlabel('Year')
+
+
+# ax.set_title('DDoS Paper Publication, Market Size, and Scale of DDoS Attack')
+ax.locator_params(integer=True)
+plt.tight_layout()
+# plt.show()
+plt.savefig('version-3.pdf')
