df05d1d8ed7a210f30eeb0f51954e5567c9e8dc4
Sichen Song <10232872+shsssc@users.noreply.github.com>
2022-08-27 17:07:31 -0700

    add page generation

---
+++ gen.py
@@ -0,0 +1,23 @@
+import csv
+import json
+from string import Template
+class MyTemplate(Template):
+    delimiter = '|'
+    idpattern = r'[a-z]+'
+
+selected_cols = [0,1,2,3,6,19]
+result = []
+with open('ddos-paper-merged - Aug23,2022.csv','r', encoding='UTF-8') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        result.append([row[x] for x in selected_cols])
+
+
+f = open('template.html','r')
+template = MyTemplate( f.read())
+f.close()
+
+out = open('index.html', 'w')
+out.write(template.substitute(paperData=json.dumps(result[1:])))
+out.close()
+#print(result[1:])
