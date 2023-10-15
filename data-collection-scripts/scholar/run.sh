d02330e3c0584dfdc66fe10b84485d63e4c4281b
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2023-10-15 11:57:11 -0700

    update run.sh

--- data-collection-scripts/scholar/run.sh
+++ data-collection-scripts/scholar/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for year in {1993..2023}
+for year in {1991..2023}
 do
 	scrapy crawl --pdb scholarv1 -a start_year=$year -a end_year=$year -o result/${year}_denial_of_service.csv --logfile result/${year}.log
 done
